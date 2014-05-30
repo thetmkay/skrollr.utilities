30a536e823c805bc9f34e761a5b10cd28c086a0c
thetmkay <george@upstatement.com>
2014-05-30 15:53:08 -0400

    version 1 with small demo working

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,25 @@
 
 	'use strict';
 
-})(jQuery);
+	var util = SkrollrUtilities.getHelper(),
+		one = util.getPos(".page-three"),
+		two = util.getPos(".page-four");
+
+	$('#test').addKeyframes([one, two], {
+		'background-color': [util.convertHex("FF0000"),util.convertHex("0000FF")]
+	});
+
+	util.onKeyframeEvent($("#test"), "data-" + one, function() {
+		console.log("Red");
+	});
+
+	util.onKeyframeEvent($("#test"), "data-" + two, function() {
+		console.log("Blue");
+	});
+
+	var s = skrollr.init({
+		forceHeight: false,
+		keyframe: util.keyframeHandlerFn
+	});
+
+})(jQuery, skrollr, SkrollrUtilities);
--- public/js/skrollr.utilities.js
+++ public/js/skrollr.utilities.js
@@ -1,12 +1,78 @@
-(function($) {
+var SkrollrUtilities = (function($) {
 
 	"use strict";
 
-	function Transform() {
+	var module = {};
+
+//===Helper Object to Expose=======
+
+	function getHelper() {
+
+		var helper = {
+			_eventHandlers: []
+		};
+
+		helper.addUnits = function(arr,unit) {
+			var suffixed_arr = [];
+			for(var elem in arr) {
+				var regex = "^.*unit$";
+				regex.replace("unit", unit);
+				regex = new RegExp(regex, "i");
+
+				if(!regex.test(elem))
+					elem += unit;
+				suffixed_arr.push(elem);
+			}
+			return suffixed_arr;
+		};
+
+		helper.convertHex = function(hex, alpha) {
+			var hex_split = hex.match(/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i);
+			if(!alpha) {
+				return "rgb(" + parseInt(hex_split[1],16) +"," + parseInt(hex_split[2],16) +"," + parseInt(hex_split[3],16) + ")";
+			}
+			return "rgba(" + parseInt(hex_split[1],16) +"," + parseInt(hex_split[2],16) +"," + parseInt(hex_split[3],16) +"," + alpha + ")";
+		};
+
+		helper.getPos = function(selector) {
+			if($(selector).length === 1)
+				return Math.floor($(selector).offset().top);
+		}
+
+		helper.onKeyframeEvent = function (element,marker, callback) {
+			//if you pass in a jquery element
+			if(element.length === 1) {
+				element = element[0];
+			}
+			$(element).attr('data-emit-events',"true");
+			var eventHandler = {
+				element: element,
+				marker: marker,
+				callback: callback
+			};
+			helper._eventHandlers.push(eventHandler);
+		};
+
+		helper.keyframeHandlerFn = function(element, name, direction){
+				name = "data-" + name.substring(4);
+			helper._eventHandlers.forEach(function(eventHandler, index) {
+				if(eventHandler.element === element && eventHandler.marker === name) {
+					eventHandler.callback(element,name,direction);
+				}
+			});
+		};
+
+		return helper;
+	};
+
+
+//===Internal String Builders
+
+	function TransformBuilder() {
 		this.trans_string = "transform:";
 	}
 
-	Transform.prototype = {
+	TransformBuilder.prototype = {
 		rotate: function(rotate_val) {
 			this.trans_string += " rotate(" + rotate_val + "deg)";
 		},
@@ -28,15 +94,12 @@
 		}
 	};
 
-	function hex_to_rgba(hex, alpha) {
-		var hex_split = hex.match(/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i);
-
-		return "rgba(" + parseInt(hex_split[1]) +"," + parseInt(hex_split[2]) +"," + parseInt(hex_split[3]) +"," + alpha + ")";
-	}
-
 	function css(attribute,value) {
 		return attribute + ":" + value + ";";
 	}
+
+
+//=======JQUERY FUNCTIONS=========
 
 	function addKeyframe(marker, animation) {
 
@@ -49,7 +112,7 @@
 			console.error("not valid marker");
 			return;
 		}
-		var transform = new Transform(),
+		var transform = new TransformBuilder(),
 			anim_string = "";
 
 		for(var prop in animation) {
@@ -73,7 +136,6 @@
 		$(this).attr(marker, anim_string);
 	}
 
-	$.fn.addKeyframe = addKeyframe;
 
 	function addKeyframes(markers, animations) {
 		var num_keyframes = markers.length;
@@ -88,5 +150,13 @@
 		}
 	}
 
+//=========EXPORT==========
+
+	$.fn.addKeyframe = addKeyframe;
 	$.fn.addKeyframes = addKeyframes;
+
+	module.getHelper = getHelper;
+
+	return module;
+
 })(jQuery);
